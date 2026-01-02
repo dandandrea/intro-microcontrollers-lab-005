d872bb99453ff2d18d796b2255c7adbe3f53cb3f
Dan D'Andrea <dan.dandrea@gmail.com>
2026-01-01 20:38:05 -0500

    Add OLED code

--- intro-microcontrollers-lab-005.ino
+++ intro-microcontrollers-lab-005.ino
@@ -1,16 +1,40 @@
-#define LED_PIN_NUMBER 8
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
 #define POT_PIN_NUMBER A0
+
+#define OLED_WIDTH_PIXELS  128
+#define OLED_HEIGHT_PIXELS 64
+
+#define OLED_I2C_ADDRESS 0x3C
+
+Adafruit_SSD1306 oled;
 
 void setup() {
   // put your setup code here, to run once:
 
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(LED_PIN_NUMBER, OUTPUT);
 
   digitalWrite(LED_BUILTIN, LOW);
-  digitalWrite(LED_PIN_NUMBER, LOW);
 
   Serial.begin(115200);
+
+  oled = Adafruit_SSD1306(OLED_WIDTH_PIXELS, OLED_HEIGHT_PIXELS, &Wire, -1);
+
+  if (!oled.begin(SSD1306_SWITCHCAPVCC, OLED_I2C_ADDRESS))
+  {
+    while (true)
+    {
+      Serial.println("ERROR: Unable to initialize OLED display");
+      delay(1000);
+    }
+  }
+
+  oled.clearDisplay();
+  oled.setTextSize(2);
+  oled.setTextColor(WHITE);
+  oled.display();
 }
 
 void loop() {
@@ -20,9 +44,8 @@ void loop() {
 
   Serial.println(adcReading);
 
-  digitalWrite(LED_PIN_NUMBER, HIGH);
-  delay(100);
-
-  digitalWrite(LED_PIN_NUMBER, LOW);
-  delay(100 + (1023 - adcReading));
+  oled.clearDisplay();
+  oled.setCursor(50, 20);
+  oled.println(adcReading);
+  oled.display();
 }
